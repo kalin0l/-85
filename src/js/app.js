dad8243e861dd5dbd794cb69b6ca0ac08916f896
kalin0l <aleksiev2494@gmail.com>
2021-12-19 17:53:33 +0200

    job done 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,33 @@
+// import { data } from "autoprefixer";
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let pokemonCount = 10
+
 
   const ul = document.querySelector("ul");
+  const getData = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}`).then(res => res.json()).then(data => {
+      console.log(data);
+      creator(data);
+      return data.results;
+    })
+
+  }
+  getData();
+  function creator(names) {
+    console.log(names.results);
+    names.results.forEach((element, i) => {
+      const lis = document.createElement("li");
+      lis.innerText = `${element.name}`
+      ul.appendChild(lis);
+
+    });
+  }
+
+
+
+
+
 });
